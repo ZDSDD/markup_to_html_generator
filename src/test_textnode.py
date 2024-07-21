125da397491c325272f12b208e14c550c9ed22c3
zdsdd <sebasadowy@gmail.com>
2024-07-21 20:36:30 +0200

    add split nodes inline

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, text_node_to_html_node
+from textnode import TextNode, TextType, split_nodes_delimiter, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -10,32 +10,61 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
     def test_url_none(self):
-        node = TextNode("this is text node", 'italic')
+        node = TextNode("this is text node", "italic")
         self.assertEqual(node.url, None)
 
     def test_text_node_to_html_node_text(self):
-        node = TextNode("This is only text", "text")
-        self.assertEqual(text_node_to_html_node(node).to_html(),'This is only text')
+        node = TextNode("This is only text", TextType.TEXT)
+        self.assertEqual(text_node_to_html_node(node).to_html(), "This is only text")
 
     def test_text_node_to_html_node_bold(self):
-        node = TextNode("This is bold text", "bold")
-        self.assertEqual(text_node_to_html_node(node).to_html(),'<b>This is bold text</b>')
+        node = TextNode("This is bold text", TextType.BOLD)
+        self.assertEqual(
+            text_node_to_html_node(node).to_html(), "<b>This is bold text</b>"
+        )
 
     def test_text_node_to_html_node_italic(self):
-        node = TextNode("This is italic text", "italic")
-        self.assertEqual(text_node_to_html_node(node).to_html(),'<i>This is italic text</i>')
-    
+        node = TextNode("This is italic text", TextType.ITALIC)
+        self.assertEqual(
+            text_node_to_html_node(node).to_html(), "<i>This is italic text</i>"
+        )
+
     def test_text_node_to_html_node_code(self):
-        node = TextNode("--c++", "code")
-        self.assertEqual(text_node_to_html_node(node).to_html(),'<code>--c++</code>')
+        node = TextNode("--c++", TextType.CODE)
+        self.assertEqual(text_node_to_html_node(node).to_html(), "<code>--c++</code>")
 
     def test_text_node_to_html_node_link(self):
-        node = TextNode(url="http://google.com", text_type="link", text='nice site')
-        self.assertEqual(text_node_to_html_node(node).to_html(),'<a href="http://google.com">nice site</a>')
+        node = TextNode(
+            url="http://google.com", text_type=TextType.LINK, text="nice site"
+        )
+        self.assertEqual(
+            text_node_to_html_node(node).to_html(),
+            '<a href="http://google.com">nice site</a>',
+        )
 
     def test_text_node_to_html_node_image(self):
-        node = TextNode(url="http://google.com/image/plant.jpg", text_type="image", text='nice plant')
-        self.assertEqual(text_node_to_html_node(node).to_html(),'<img src="http://google.com/image/plant.jpg" alt="nice plant"></img>')
-    
+        node = TextNode(
+            url="http://google.com/image/plant.jpg",
+            text_type=TextType.IMAGE,
+            text="nice plant",
+        )
+        self.assertEqual(
+            text_node_to_html_node(node).to_html(),
+            '<img src="http://google.com/image/plant.jpg" alt="nice plant"></img>',
+        )
+
+    def test_split_nodes_delimeter(self):
+        node = TextNode("This is text with a `code block` word", TextType.CODE)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+
+        self.assertListEqual(new_nodes, expected)
+
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,15 @@
 from htmlnode import LeafNode
+import re
+from enum import Enum, auto
 
-
+class TextType(Enum):
+    TEXT = auto()
+    BOLD = auto()
+    ITALIC = auto()
+    CODE = auto()
+    LINK = auto()
+    IMAGE = auto()
+    
 class TextNode:
     def __init__(self, text: str, text_type: str, url=None) -> None:
         self.text = text
@@ -19,19 +28,44 @@ class TextNode:
 
 
 def text_node_to_html_node(text_node: TextNode):
-
-    match (text_node.text_type):
-        case "text":
+    match text_node.text_type:
+        case TextType.TEXT:
             return LeafNode(value=text_node.text)
-        case "bold":
+        case TextType.BOLD:
             return LeafNode(tag="b", value=text_node.text)
-        case "italic":
+        case TextType.ITALIC:
             return LeafNode(tag="i", value=text_node.text)
-        case "code":
+        case TextType.CODE:
             return LeafNode(tag="code", value=text_node.text)
-        case "link":
-            return LeafNode(tag="a", value=text_node.text, props={"href":text_node.url})
-        case "image":
+        case TextType.LINK:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE:
             return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
         case _:
             raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType)-> list[TextNode]:
+    # Escape delimiter for regex
+    escaped_delimiter = re.escape(delimiter)
+    nodes = []
+    for node in old_nodes:
+        input_text = node.text
+        # Construct the regex pattern dynamically
+        pattern = re.compile(f'{escaped_delimiter}(.*?){escaped_delimiter}')
+        
+        last_end = 0
+        
+        for match in pattern.finditer(input_text):
+            # Add the text before the delimiter
+            if match.start() > last_end:
+                nodes.append(TextNode(input_text[last_end:match.start()], TextType.TEXT))
+            
+            # Add the text between delimiters
+            nodes.append(TextNode(match.group(1), text_type=text_type))
+            last_end = match.end()
+        
+        # Add the remaining text after the last delimiter
+        if last_end < len(input_text):
+            nodes.append(TextNode(input_text[last_end:], TextType.TEXT))
+    
+    return nodes
